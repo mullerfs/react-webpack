c4a95a1639f42c745e780ad3914b6eb30cc82bcc
muller <muller.udi@gmail.com>
2021-04-04 12:46:04 -0300

    adequação para react

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('Hello World!');
+import React from "react";
+import ReactDOM from "react-dom";
+
+import MyImage from './assets/unspash.jpg';
+import './index.scss';
+
+const App = () => {
+  return (
+    <div>
+        <div>Welcome to my-webpack-react-starter</div>
+        <img src={MyImage} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,66 @@
-// Generated using webpack-cli http://github.com/webpack-cli
 const path = require('path');
+const autoprefixer = require('autoprefixer');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        chunkFilename: '[id].js',
+        publicPath: ''
     },
-    devServer: {
-        open: true,
-        host: 'localhost',
+    resolve: {
+        extensions: ['.js', '.jsx']
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html',
-        }),
-
-        // Add your plugins here
-        // Learn more obout plugins from https://webpack.js.org/configuration/plugins/
-    ],
     module: {
         rules: [
             {
-                test: /\\.(js|jsx)$/,
+                test: /\.js$/,
                 loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    { loader: 'style-loader' },
+                    { 
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                localIdentName: "[name]__[local]___[hash:base64:5]",
+                            },														
+                            sourceMap: true
+                        }
+                     },
+                     { 
+                         loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    [ 'autoprefixer', {}, ],
+                                ],
+                            },
+                        }
+                      }
+                ]
             },
             {
                 test: /\.s[ac]ss$/i,
                 use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
             },
             {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader', 'postcss-loader'],
-            },
-            {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/,
                 type: 'asset',
-            },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
-        ],
+            }
+        ]
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: __dirname + '/src/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        })
+    ]
 };
